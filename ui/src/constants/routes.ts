abb02b69cb5eb38b8324595bc23e4203669de914
Luis Daniel Mendez Castellanos <syi216618@gmail.com>
2025-03-29 19:26:22 -0700

    Navbar routes have been updated

--- ui/src/constants/routes.ts
+++ ui/src/constants/routes.ts
@@ -1,7 +1,6 @@
 export enum Navigation {
   HOME = "/",
   AIAGENT = "/agent",
-  ACCOUNT = "/account",
   DEPOSIT = "/deposit",
   WITHDRAW = "/withdraw",
   JOBS = "/jobs",
@@ -15,7 +14,6 @@ export type NavigationLinks = {
 
 export const LINKS_HEADER: NavigationLinks[] = [
   { href: Navigation.AIAGENT, label: "Ai Agent" },
-  { href: Navigation.ACCOUNT, label: "Balances" },
   { href: Navigation.DEPOSIT, label: "Deposit" },
   { href: Navigation.HOME, label: "Swap" },
   { href: Navigation.WITHDRAW, label: "Withdraw" },
